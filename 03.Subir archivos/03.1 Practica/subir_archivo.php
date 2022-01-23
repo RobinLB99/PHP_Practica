78d78991dbdf4d2562491b347c1188c79b4b13dc
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-01-23 13:52:46 -0500

    Practica - subir archivo al servidor

--- 03.Subir archivos/03.1 Practica/subir_archivo.php
+++ 03.Subir archivos/03.1 Practica/subir_archivo.php
@@ -4,6 +4,7 @@
     $destino = "archivos/".$_FILES["archivo_selec"]["name"];
 
     if ($_FILES["archivo_selec"]["type"] === "application/vnd.ms-excel") {
+        move_uploaded_file($archivo, $destino);
         echo "El archivo a sido subido con exito.";
     }
     else {
