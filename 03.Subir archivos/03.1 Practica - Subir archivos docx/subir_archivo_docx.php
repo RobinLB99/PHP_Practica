ce0d33ed36fb21b23f5f82906fbc394927680830
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-01-23 23:18:41 -0500

    Subir archivo DOCX al servidor

---
+++ 03.Subir archivos/03.1 Practica - Subir archivos docx/subir_archivo_docx.php
@@ -0,0 +1,12 @@
+<?php
+    $archivo = $_FILES["file_up"]["tmp_name"]; // Archivo cargado.
+    $directorio = "Archivos DOCX/".$_FILES["file_up"]["name"]; // Direccion donde se va a guardar y nombre con que se guarda.
+
+    if($_FILES["file_up"]["type"] === "application/vnd.openxmlformats-officedocument.wordprocessingml.document"){
+        move_uploaded_file($archivo, $directorio);
+        echo "El archivo se cargo con exito al servidor";
+    }
+    else{
+        header("Location: enviar_archivo.php?error=true");
+    }
+?>
